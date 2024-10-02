dba2773d5d211ef5a3235a1b704f8031e30a0cff
Rion Tuscano <riontuscano@gmail.com>
2024-10-02 07:12:19 +0530

    done

--- static/js/predictor.js
+++ static/js/predictor.js
@@ -2,7 +2,20 @@ const menuicon = document.querySelector("#menu-icon");
 const menu = document.querySelector(".block");
 const menuclose = document.querySelector("#cancel");
 const submenu = document.getElementById("subwrap");
+const progressValueElement = document.querySelector('.progress-value');
+const progressCircleElement = document.querySelector('.progress-circle::after');
+const slider = document.getElementById('percentageSlider');
+    
+const fill = document.querySelector('.fill');
+    const percentageText = document.querySelector('.percentage-text');
 
+    function updatePercentage() {
+      const value = slider.value;
+      fill.style.height = `${value}%`;
+      percentageText.textContent = `${value}%`;
+    }
+
+    slider.addEventListener('input', updatePercentage);
 
 function toggleprofile() {
     submenu.classList.toggle("open-menu");
@@ -28,6 +41,15 @@ function activateMenu() {
     menu.classList.remove("active");
   };
 }
+// function updateProgressBar(percentage) {
+//   const progressValueElement = document.querySelector('.progress-value');
+//   const progressCircleElement = document.querySelector('.progress-circle::after');
 
+//   progressValueElement.textContent = percentage + '%';
+//   progressCircleElement.style.transform = `rotate(${percentage * 3.6}deg)`;
+// }
 
+// updateProgressBar(75); 
+
+updateProgressBar(75); 
 activateMenu()
